8aee87fde827980ceebee0eb87b4db5279d199af
Humayun <humayun0156@gmail.com>
2016-05-25 10:43:38 +0600

    UVA 01124 Celebrity Jeopardy

---
+++ src/main/java/uva/UVA_01124.java
@@ -0,0 +1,15 @@
+package uva;
+
+import java.util.Scanner;
+
+/**
+ * @author humayun
+ */
+public class UVA_01124 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
